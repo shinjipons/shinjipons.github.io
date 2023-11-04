519c3dde7ad2d5c2bfbf8dfbe5f0ea196246c8cb
Shinji Pons <accounts@shinjipons.com>
2023-11-04 16:39:16 +0100

    Added Apple Bubble Letter data

--- python/pages_data.py
+++ python/pages_data.py
@@ -1,8 +1,10 @@
 # Content to insert in the placeholders
 pages = {
-    "louvre-abu-dhabi": {
-        "title": "Louvre Abu Dhabi",
-        "paragraph_1": "While at Ateliers Jean Nouvel, I worked on the typographic signage and icon design for the Louvre Abu Dhabi museum. Under the the creative direction of Philippe Apeloig and Clovis Vallois, I sketched, designed and mocked up several physical designs for the typographic system in French, English and Arabic.",
-        "paragraph_2": "The graphic designers were in constant collaboration with the architects to decide the size and location of the signage, but also to remove walls where needed to increase signage visibility."
+    "apple-letter": {
+        "title": "Apple Bubble Letter",
+        "paragraph_1": "As part of an interview loop, I did this bubble letter of a capital R. I haven't yet solved it without making some major compromises to the surfaces at the intersection of the letter's stems. Perhaps when I have more free time, I will give it another try.",
+        "paragraph_2": "The brief was to model a bubble letter with a circular profile while avoiding flat areas at the intersection and, of course, maintaining perfect continuity and clean geometry. The real challenge is that avoiding flat areas means making some compromises in continuity.",
+        "paragraph_3": "The biggest challenge in this exercise to avoid flat planes in the transition areas where the main pipes of the bubble letter meet. If I were to only aim for G2 continuity and didn't care too much for heavy surfaces, it could work. But what would be the fun in that?",
+        "paragraph_4" : "While this result looks OK, there is lots I can improve on. The transition areas are too flat and I went for too much of a pill-shaped profile. If you are willing to teach me the proper techniques, please get in touch."
     }
 }
