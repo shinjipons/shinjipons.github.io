97c7bd7208b6fb05e4aba90b642708b9b6cfa006
Shinji Pons <accounts@shinjipons.com>
2023-11-09 22:04:54 +0100

    added louvre

--- python/pages_data.py
+++ python/pages_data.py
@@ -7,7 +7,7 @@ pages = {
         "paragraph_2": "The brief was to model a bubble letter with a circular profile while avoiding flat areas at the intersection and, of course, maintaining perfect continuity and clean geometry. The real challenge is that avoiding flat areas means making some compromises in continuity.",
         "paragraph_3": "The biggest challenge in this exercise to avoid flat planes in the transition areas where the main pipes of the bubble letter meet. If I were to only aim for G2 continuity and didn't care too much for heavy surfaces, it could work. But what would be the fun in that? While this result looks OK, there is lots I can improve on.",
         "paragraph_4": "The transition areas are too flat and I went for too much of a pill-shaped profile. If you are willing to teach me the proper techniques, please get in touch.",
-        "image_folder": "images/apple-letter"
+        "image_folder": "images/apple-letter/"
     },
     "test-missing-paragraphs": {
         "title": "Test Project with missing paragraph",
@@ -17,5 +17,14 @@ pages = {
         "paragraph_3": "This is another paragraph",
         "paragraph_4": "", # left empty on purpose
         "image_folder": "" # left empty on purpose
+    }, 
+    "louvre-abu-dhabi": {
+        "title" : "Louvre Abu Dhabi",
+        "tagline": "Signage and icon design",
+        "paragraph_1": "While at Ateliers Jean Nouvel, I worked on the typographic signage and icon design for the Louvre Abu Dhabi museum. Under the the creative direction of Philippe Apeloig and Clovis Vallois, I sketched, designed and mocked up several physical designs for the typographic system in French, English and Arabic.",
+        "paragraph_2": "The graphic designers were in constant collaboration with the architects to decide the size and location of the signage, but also to remove walls where needed to increase signage visibility.",
+        "paragraph_3": "To accompany the building's unique design, Philippe Apeloig had devised a system using the dome's light patterns. The challenge was to make the icons understandable from every culture.",
+        "paragraph_4": "After many iterations, the result was an elegant and timeless system for a global audience. I am still very proud to have contributed to this project to this day.",
+        "image_folder": "images/louvre-abu-dhabi/"
     }
 }
