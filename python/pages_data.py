64263f57488b6698347854ed711f155037f2c3de
Shinji Pons <accounts@shinjipons.com>
2023-11-12 16:53:15 +0100

    Added more projects

--- python/pages_data.py
+++ python/pages_data.py
@@ -2,29 +2,62 @@
 pages = {
     "apple-letter": {
         "title": "Apple Bubble Letter",
-        "tagline": "Spider Pig is real!",
+        "tagline": "Modelling challenge for external company",
+        "client": "Personal Project",
+        "type": "Alias Modelling",
+        "year": "2020",
         "paragraph_1": "As part of an interview loop, I did this bubble letter of a capital R. I haven't yet solved it without making some major compromises to the surfaces at the intersection of the letter's stems. Perhaps when I have more free time, I will give it another try.",
         "paragraph_2": "The brief was to model a bubble letter with a circular profile while avoiding flat areas at the intersection and, of course, maintaining perfect continuity and clean geometry. The real challenge is that avoiding flat areas means making some compromises in continuity.",
         "paragraph_3": "The biggest challenge in this exercise to avoid flat planes in the transition areas where the main pipes of the bubble letter meet. If I were to only aim for G2 continuity and didn't care too much for heavy surfaces, it could work. But what would be the fun in that? While this result looks OK, there is lots I can improve on.",
         "paragraph_4": "The transition areas are too flat and I went for too much of a pill-shaped profile. If you are willing to teach me the proper techniques, please get in touch.",
         "image_folder": "images/apple-letter/"
     },
-    "test-missing-paragraphs": {
-        "title": "Test Project with missing paragraph",
-        "tagline": "Spider Pig is real!",
-        "paragraph_1": "This is a paragraph",
-        "paragraph_2": "This is another paragraph",
-        "paragraph_3": "This is another paragraph",
-        "paragraph_4": "", # left empty on purpose
-        "image_folder": "" # left empty on purpose
-    }, 
     "louvre-abu-dhabi": {
-        "title" : "Louvre Abu Dhabi",
-        "tagline": "Signage and icon design",
+        "title": "Louvre Abu Dhabi",
+        "tagline": "Signage inspired by architecture",
+        "client": "Ateliers Jean Nouvel",
+        "type": "Graphic Design",
+        "year": "2012",
         "paragraph_1": "While at Ateliers Jean Nouvel, I worked on the typographic signage and icon design for the Louvre Abu Dhabi museum. Under the the creative direction of Philippe Apeloig and Clovis Vallois, I sketched, designed and mocked up several physical designs for the typographic system in French, English and Arabic.",
         "paragraph_2": "The graphic designers were in constant collaboration with the architects to decide the size and location of the signage, but also to remove walls where needed to increase signage visibility.",
         "paragraph_3": "To accompany the building's unique design, Philippe Apeloig had devised a system using the dome's light patterns. The challenge was to make the icons understandable from every culture.",
         "paragraph_4": "After many iterations, the result was an elegant and timeless system for a global audience. I am still very proud to have contributed to this project to this day.",
         "image_folder": "images/louvre-abu-dhabi/"
+    }, 
+    "lego-digital-designer-pro": {
+        "title": "LEGO Digital Designer Pro",
+        "tagline": "CAD software for LEGO sets",
+        "client": "The LEGO Group",
+        "type": "UX Design",
+        "year": "2021",
+        "paragraph_1": "While at the LEGO Group, I had the privilege of working on designing tools and workflows for LEGO's in-house software for designing virtual LEGO sets and authoring digital building instructions. Along with another product designer, I was tasked with improving efficiency and reducing costs for the business by streamlining processes and automating tedious tasks.",
+        "image_folder": "images/ldd-pro",
+        "paragraph_2": "LEGO's design process is hindered by manual labor, as brick counting and instruction creation are time-consuming and error-prone. Streamlining these steps could improve efficiency and reduce risks to the business. The LEGO Group was in the transition phase from relying on an outdated Maya plugin to LEGO Digital Designer Pro. However, redesigning the entire pipeline came with huge challenges in supporting customers during this transition phase without disrupting the normal flow of business.",
+        "paragraph_3": "The design process of LEGO sets and building instructions was ripe for automation. Many of the steps, such as determining the manufacturing cost of a set, detecting duplicate assemblies, and creating marketing material, were still done manually by humans instead of machines and software. The principle was to let computers do as much of the boring work as possible.",
+        "paragraph_4": "Some of my work ended up saving LEGO model designers a lot of time when building their sets. While most of the designs were not the most innovative, the basics were much more efficient and quicker than with the Maya plugin."
+    }, 
+    "alias-subd": {
+        "title": "Autodesk Alias SubD",
+        "tagline": "Making Alias better for everyone",
+        "client": "Autodesk",
+        "type": "UX Design",
+        "year": "2019",
+        "image_folder": "images/alias-subd",
+        "paragraph_1": "Autodesk Alias is an industry-leading computer-aided design software application that empowers designers to create high-quality, manufacturable surfaces. This powerful tool is widely used in automotive and industrial design to bring aesthetically pleasing and feasible objects to life. As a digital product designer, I had the privilege of designing the user experience for the SubD toolset within Alias. My work ensured seamless integration with the existing NURBS tools, making the entire design process more efficient and intuitive.",
+        "paragraph_2": "The main issue with Alias is its steep learning curve. New users have to face a lack of available educational content to bridge their skill gap in NURBS modelling. This limited the growth of Alias.",
+        "paragraph_3": "Thanks to these tools that work just like subdivision surfaces, new users can produce 3D models that can be refined and reworked by more experienced users without ever exporting anything, or compromising on quality.",
+        "paragraph_4": "Many customers now enjoy shorter delivery times for their concept models and Alias is now more than ever horizontally integrated in the automotive design studio."
+    }, 
+    "dynamo-for-alias": {
+        "title": "Dynamo for Alias",
+        "tagline": "Augmenting Alias with generative algorithms",
+        "client": "Autodesk",
+        "type": "UX Design",
+        "year": "2017",
+        "image_folder": "images/dynamo-for-alias",
+        "paragraph_1": "While at Autodesk, I worked on the integration of Dynamo, a visual programming environment, into Alias. This workflow improvement empowered designers to leverage scripting to create and iterate on complex surfaces that are otherwise too time-consuming to model using the graphical user interface.",
+        "paragraph_2": "Some automotive surfaces increasingly feature intricate details such as front grilles. To speed up the design of these complex parts, Dynamo brings a visual programming environment to automate the task of producing geometry.",
+        "paragraph_3": "The main Issues were the surface quality of Dynamo surfaces and switching tools in Alias broke the live connection with Dynamo. At the end of the project, I was able to recreate the Mac Pro grille entirely using Dynamo.",
+        "paragraph_4": "Even while in a prototype phase, Dynamo for Alias was successfully used on a concept vehicle design for Tata, the 45X, which drastically reduced creative waste and modelling hours."
     }
 }
