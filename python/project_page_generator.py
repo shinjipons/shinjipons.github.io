734233aeddff80b7c2608ca66e3ba22aa3f34f24
Shinji Pons <accounts@shinjipons.com>
2023-11-28 18:49:49 +0100

    added favicon on all pages

--- python/project_page_generator.py
+++ python/project_page_generator.py
@@ -43,6 +43,9 @@ def generate_page(page_key, page_data):
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
     <title>Shinji Pons - {title}</title>
     <link rel="stylesheet" href="styles.css">
+    <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png">
+	<link rel="apple-touch-icon" sizes="180x180"    href="/icons/favicon-ios.png">
 </head>
 <body>
 
