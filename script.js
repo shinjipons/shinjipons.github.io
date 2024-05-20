15ef06df7088ae2dfcf5bed945751d4be82a658f
Shinji Pons <accounts@shinjipons.com>
2024-05-21 00:33:55 +0200

    Scrambler effect done

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const menu = document.getElementById('menuPage');
 const trigger = document.getElementById('menuTrigger');
+const textToModify = document.getElementById('text-to-scramble');
+var finalMessage = "available for work";
+var characters = "-abcdefghijklmnopqrstuvwxyz0123456789, [](){}";
 
 trigger.addEventListener('click', function() {
     menuTriggerClickHandler();
@@ -21,3 +24,32 @@ function menuTriggerClickHandler() {
         menu.classList.add('menu-closed');
     }
 }
+
+function messageInitializer(inputMessage) {
+    var initialMessage = "";
+    for (var i = 0; i < finalMessage.length; i++) {
+        initialMessage += characters[Math.floor(Math.random() * characters.length)]
+    }
+    textToModify.innerHTML = initialMessage;
+    // return isInitialized = trye
+}
+
+function messageDecoder(inputMessage) {
+    for (var i = 0; i < finalMessage.length; i++) {
+        var randomIndex = Math.floor(Math.random() * inputMessage.length);
+        var outputMessage = inputMessage.slice(0, randomIndex) + finalMessage[randomIndex] + inputMessage.slice(randomIndex + 1)
+        textToModify.innerHTML = outputMessage;
+    }
+}
+
+messageInitializer(textToModify.innerHTML);
+
+const intervalId = setInterval(() => {
+    messageDecoder(textToModify.innerHTML)
+
+    if (textToModify.innerHTML == finalMessage) {
+        clearInterval(intervalId);
+        textToModify.classList.add('availability-confirmed');
+    }
+
+}, 3);
