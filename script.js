88478f2f998544749e16976116ce40853d83801b
Shinji Pons <accounts@shinjipons.com>
2024-05-20 19:26:12 +0200

    Started working on the javascript menu

---
+++ script.js
@@ -0,0 +1,20 @@
+const menu = document.getElementById('menuPage');
+const trigger = document.getElementById('menuTrigger');
+
+trigger.addEventListener('click', function() {
+    // console.log('working');
+    console.log(menu.classList)
+})
+
+function menuTriggerClickHandler() {
+    // check if closed
+    if (menu.classList.contains('menu-closed')) {
+        // then "open" the menu by adding and removing the right class
+        menu.classList.remove('menu-closed');
+        menu.classList.add('menu-open');
+    } else {
+        // then "close" the menu by adding and removing the right class
+        menu.classList.remove('menu-open');
+        menu.classList.add('menu-closed');
+    }
+}
