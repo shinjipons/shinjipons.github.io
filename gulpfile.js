103b4226c865334d14b79d7fb49bb7867a215c9b
Shinji Pons <github@shinjipons.com>
2024-07-14 00:23:23 +0200

    styles now work

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,33 @@ const sass = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync').create();
 const fileInclude = require('gulp-file-include');
 
+// paths
+const paths = {
+    scss: {
+        src:'src/scss/*.scss',
+        dest: 'dist/css'
+    }
+}
+
 // Compile SCSS to CSS
+function style() {
+    return gulp.src(paths.scss.src)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(paths.scss.dest))
+        .pipe(browserSync.stream());
+}
 
 // Watch everything and serve
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+    gulp.watch(paths.scss.src, style);
+}
 
-
-
-
-
-
-// inside of package.json, `npm start` triggers `build` defined below...
+// Define task in series
 const build = gulp.series(style, watch);
 
 // Export tasks
