eacdc0b2935108d3e659cd52b5eb2db3c0b67205
Shinji Pons <github@shinjipons.com>
2024-07-23 23:14:52 +0200

    Update project_page_generator.py

--- src/python/project_page_generator.py
+++ src/python/project_page_generator.py
@@ -2,15 +2,12 @@ import os
 from pages_data import pages as pages_data
 
 # Read the shared footer from a file
-with open('python/shared_nav.html', 'r') as file:
+with open('src/python/shared_nav.html', 'r') as file:
     shared_nav = file.read()
-
-# Read the shared footer from a file
-# with open('python/shared_footer.html', 'r') as file:
-#     shared_footer = file.read()
 
 # Function to get all webp images from a specific folder
 def get_image_tags(image_folder):
+
     # Get all webp images and sort them
     files = sorted([f for f in os.listdir(image_folder) if f.endswith('.webp')])
 
@@ -36,7 +33,6 @@ def generate_page(page_key, page_data):
 
     # Include the shared HTML block, the first image, and other image tags in the page data dictionary
     page_data['shared_nav'] = shared_nav
-    # page_data['shared_footer'] = shared_footer
     page_data['first_image'] = first_image_tag
     page_data['other_images'] = '\n'.join(other_image_tags)
 
@@ -47,53 +43,41 @@ def generate_page(page_key, page_data):
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
     <title>Shinji Pons | Product Designer of 3D Tools & Beyond | {title}</title>
-    <meta name="description" content="{description}">
-    <link rel="stylesheet" href="dist/css/styles.css">
-
-    <!-- Facebook Meta Tags -->
-    <meta property="og:url" content="https://shinjipons.com">
-    <meta property="og:type" content="website">
-    <meta property="og:title" content="Shinji Pons | Product Designer of 3D Tools & Beyond | {title}">
-    <meta property="og:description" content="{description}">
-    <meta property="og:image" content="https://www.shinjipons.com/images/opengraph.jpg">
-
-    <!-- Twitter Meta Tags -->
-    <meta name="twitter:card" content="summary_large_image">
-    <meta property="twitter:domain" content="shinjipons.com">
-    <meta property="twitter:url" content="https://shinjipons.com">
-    <meta name="twitter:title" content="Shinji Pons | Product Designer of 3D Tools & Beyond | {title}">
-    <meta name="twitter:description" content="{description}">
-    <meta name="twitter:image" content="https://www.shinjipons.com/images/opengraph.jpg">
-
-    <!-- Favicon Tags -->
-    <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png">
-    <link rel="apple-touch-icon" sizes="180x180"    href="/icons/favicon-ios.png">
+    <meta name="description"          content="{description}">
+    <link rel="stylesheet" href="../dist/css/styles.css">
+    <meta property="og:url"           content="https://shinjipons.com">
+    <meta property="og:type"          content="website">
+    <meta property="og:title"         content="Shinji Pons | Product Designer of 3D Tools & Beyond | {title}">
+    <meta property="og:description"   content="{description}">
+    <meta property="og:image"         content="https://www.shinjipons.com/images/opengraph.jpg">
+    <meta name="twitter:card"         content="summary_large_image">
+    <meta property="twitter:domain"   content="shinjipons.com">
+    <meta property="twitter:url"      content="https://shinjipons.com">
+    <meta name="twitter:title"        content="Shinji Pons | Product Designer of 3D Tools & Beyond | {title}">
+    <meta name="twitter:description"  content="{description}">
+    <meta name="twitter:image"        content="https://www.shinjipons.com/images/opengraph.jpg">
+    <link rel="icon" type="image/png" sizes="32x32"   href="/icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16"   href="/icons/favicon-16x16.png">
+    <link rel="apple-touch-icon"      sizes="180x180" href="/icons/favicon-ios.png">
 </head>
 <body>
-
     <main>
         <!-- Left column -->
         <div class="left-column">
             {shared_nav}
-
             <ul class="monospace">About
                 <li>{paragraph_1}</li>
                 <li>{paragraph_2}</li>
             </ul>
-
             <ul class="monospace">My role
                 <li>{paragraph_3}</li>
                 <li>{paragraph_4}</li>
             </ul>
-
         </div>
-
         <!-- Right column -->
         <div class="right-column">
             {first_image}
             {other_images}
-
             <!-- Next project -->
             <!-- <section class="next-project">
                 <a href="" id="next-project-link">
@@ -103,11 +87,8 @@ def generate_page(page_key, page_data):
                 </a>
             </section> -->
         </div>
-
     </main>
-
-    <script type="text/javascript" src="script.js"></script>
-
+    <script type="text/javascript" src="../dist/js/script.js"></script>
 </body>
 </html>
 """
@@ -116,7 +97,6 @@ def generate_page(page_key, page_data):
 
     with open(f"{page_key}.html", "w") as html_file:
         html_file.write(formatted_html)
-    print(f"{page_key.capitalize()} page created successfully!")
 
 # Generate all pages
 for page_key, page_data in pages_data.items():
