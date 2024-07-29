30edb27fa6ab8d770dccadb85d8b492fc2dea35a
Shinji Pons <github@shinjipons.com>
2024-07-29 18:21:02 +0200

    dist commit

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const articleOutline = document.getElementById('article-outline');
+    const headers = document.querySelectorAll('h1[id^="chapter-"], h2[id^="chapter-"], h3[id^="chapter-"]');
+
+    headers.forEach(header => {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        a.href = `#${header.id}`;
+        a.textContent = header.textContent;
+        li.appendChild(a);
+        articleOutline.appendChild(li);
+    });
+});
+
 // Code to add a random project to the bottom of each project page
 
 const projects = [
@@ -85,5 +99,3 @@ function addNextProject() {
 }
 
 addNextProject();
-
-// console.log("hello world 12");
