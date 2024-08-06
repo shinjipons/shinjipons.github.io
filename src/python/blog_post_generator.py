90a2f9ee0785c1194578ebd9a0c1d9ea185f8983
Shinji Pons <github@shinjipons.com>
2024-08-06 19:59:45 +0200

    everything works

--- src/python/blog_post_generator.py
+++ src/python/blog_post_generator.py
@@ -1,81 +1,31 @@
 import os
 
 # The global variables
-OUTPUT_FOLDER = 'dist/blog'
-markdown_src_directory = "src/markdown"
-ul_list_item_class = "bullet-point-list-item"
-ol_list_item_class = "numbered-list-item"
-quote_line_class = "blog-callout-block"
-code_block_class = "blog-code-block"
-blog_article_item_class = "blog-article-item"
+OUTPUT_FOLDER_BLOG_POSTS = 'dist/blog'
+OUTPUT_FOLDER_BLOG_INDEX = 'dist/'
+markdown_src_directory   = "src/markdown"
+
+ul_list_item_class       = "bullet-point-list-item"
+ol_list_item_class       = "numbered-list-item"
+quote_line_class         = "blog-callout-block"
+code_block_class         = "blog-code-block"
+blog_article_item_class  = "blog-article-item"
+post_date_class          = "blog-post-date"
 
 # Create the output folder if it doesn't exist
-if not os.path.exists(OUTPUT_FOLDER):
-    os.makedirs(OUTPUT_FOLDER)
+if not os.path.exists(OUTPUT_FOLDER_BLOG_POSTS):
+    os.makedirs(OUTPUT_FOLDER_BLOG_POSTS)
 
-# Find all the markdown files inside of a directory
+# Utility functions
 def get_all_markdown_filepaths(directory_path):
-
-    # Ensure the directory exists
     if not os.path.isdir(directory_path):
         raise ValueError(f"The specified path is not a directory: {directory_path}")
 
     # Ger all markdown files in the directory
     markdown_filepaths = [f"{directory_path}/{f}" for f in os.listdir(directory_path) if f.endswith('.md')]
 
-    # return markdown_filepaths, markdown_file_names
     return markdown_filepaths
 
-# Main function thath does the magic
-def generate_html_from_markdown(markdown_filepath):
-
-    with open(markdown_filepath, 'r') as file:
-        markdown_content = file.read()
-
-        # Split the markdown content into lines
-        lines = markdown_content.split('\n')
-
-        for line in lines:
-            if len(line) == 0 or line.startswith(("---", "date:", "description: ", "title:", "author:")): # Empty lines and front matter
-                pass
-            elif line.startswith('#'): # Headers
-                header_parts = line.split(' ')
-                header_level = len(header_parts[0])
-                header_text = ' '.join(header_parts[1:])
-                header_html = f'<h{header_level}>{header_text}</h{header_level}>' # need to add the ids? or do it via javascript
-                html_lines.append(header_html)
-            elif line.startswith('- '): # Unordered list items
-                bullet_point_text = line.strip('- ')
-                bullet_point_html = f"""\t<li class="{ul_list_item_class}">{bullet_point_text}</li>"""
-                html_lines.append(bullet_point_html)
-            elif line.startswith(("1", "2", "3", "4", "5", "6", "7", "8", "9")): # Ordered lists (ugly but works)
-                numbered_list_item_parts = line.split(". ")
-                numbered_list_item_text = ' '.join(numbered_list_item_parts[1:])
-                numbered_list_item_html = f"""\t<li class="{ol_list_item_class}">{numbered_list_item_text}</li>"""
-                html_lines.append(numbered_list_item_html)
-            elif line.startswith('!('): # Media (images and videos)
-                media_caption = extract_parentheses(line)
-                media_url = extract_brackets(line)
-                if media_url.endswith('.mp4'): # Videos, without captions for now
-                    video_html = f"""<video autoplay loop><source src="{media_url}" type="video/mp4" /></video>"""
-                    html_lines.append(video_html)
-                elif len(media_caption) == 0: # image without caption
-                    image_html = f"""<picture><img src="{media_url}"></picture>"""
-                    html_lines.append(image_html)
-                else: # image with caption
-                    image_html = f"""<picture><img src="{media_url}"><p class="caption">{media_caption}</p></picture>"""
-                    html_lines.append(image_html)
-            elif line.startswith('> '): # Quotes
-                quote_text = line.lstrip("> ")
-                quote_html = f"""<div class="{quote_line_class}"><p>{quote_text}</p></div>"""
-                html_lines.append(quote_html)
-            else: # Normal text paragraphs
-                line_html = f"""<p>{line}</p>"""
-                html_lines.append(line_html)
-
-    return html_lines
-
-# Utility functions
 def extract_parentheses(string):
     start = string.find('(')
     if start != -1: # Check if '(' is found
@@ -142,6 +92,26 @@ def replace_bold(text):
             i += 1
     return result
 
+def replace_italics(text):
+    i = 0
+    result = ""
+    while i < len(text):
+        if text[i:i+2] == '*':
+            j = i + 2
+            while j < len(text) and text[j:j+2] != '*':
+                j += 1
+            if j < len(text):
+                # Add <b> and </b> around the bold text
+                result += '<i>' + text[i+2:j] + '</i>'
+                i = j + 2
+            else:
+                result += text[i:]
+                break
+        else:
+            result += text[i]
+            i += 1
+    return result
+
 def wrap_list_with_prefix(input_list, prefix, opening_html_tag):
     def create_after_item(item):
         return item[:1] + "/" + item[1:]
@@ -181,7 +151,79 @@ def get_frontmatter_item(markdown_filepath, frontmatter_item):
 
     return result
 
-def generate_page(post_title, post_description, html_lines):
+def format_date(date_string):
+    year, month, day = date_string.split('-')
+    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    month_name = months[int(month) - 1]
+
+    # Add original suffix to day
+    day_int = int(day)
+    if 4 <= day_int <= 20 or 24 <= day_int <= 30:
+        suffix = "th"
+    else:
+        suffix = ["st", "nd", "rd"][day_int % 10 - 1]
+
+    formatted_date = f"{month_name} {day_int}{suffix}, {year}"
+
+    return formatted_date
+
+# Function for converting the markdown into html lines
+def generate_html_from_markdown(markdown_filepath):
+
+    with open(markdown_filepath, 'r') as file:
+        markdown_content = file.read()
+
+        # Split the markdown content into lines
+        lines = markdown_content.split('\n')
+
+        for line in lines:
+            if len(line) == 0 or line.startswith(("---", "description: ", "title:", "author:")): # Empty lines and front matter
+                pass
+            elif line.startswith("date:"):
+                post_date_text = format_date(line.lstrip("date: "))
+                post_date_html = f"""<p class="{post_date_class}">{post_date_text}</p>"""
+            elif line.startswith('#'): # Headers
+                header_parts = line.split(' ')
+                header_level = len(header_parts[0])
+                header_text = ' '.join(header_parts[1:])
+                header_html = f'<h{header_level}>{header_text}</h{header_level}>' # need to add the ids? or do it via javascript
+                html_lines.append(header_html)
+            elif line.startswith('- '): # Unordered list items
+                bullet_point_text = line.strip('- ')
+                bullet_point_html = f"""\t<li class="{ul_list_item_class}">{bullet_point_text}</li>"""
+                html_lines.append(bullet_point_html)
+            elif line.startswith(("1", "2", "3", "4", "5", "6", "7", "8", "9")): # Ordered lists (ugly but works)
+                numbered_list_item_parts = line.split(". ")
+                numbered_list_item_text = ' '.join(numbered_list_item_parts[1:])
+                numbered_list_item_html = f"""\t<li class="{ol_list_item_class}">{numbered_list_item_text}</li>"""
+                html_lines.append(numbered_list_item_html)
+            elif line.startswith('!('): # Media (images and videos)
+                media_caption = extract_parentheses(line)
+                media_url = extract_brackets(line)
+                if media_url.endswith('.mp4'): # Videos, without captions for now
+                    video_html = f"""<video autoplay loop><source src="{media_url}" type="video/mp4" /></video>"""
+                    html_lines.append(video_html)
+                elif len(media_caption) == 0: # image without caption
+                    image_html = f"""<picture><img src="{media_url}"></picture>"""
+                    html_lines.append(image_html)
+                else: # image with caption
+                    image_html = f"""<picture><img src="{media_url}"><p class="caption">{media_caption}</p></picture>"""
+                    html_lines.append(image_html)
+            elif line.startswith('> '): # Quotes
+                quote_text = line.lstrip("> ")
+                quote_html = f"""<div class="{quote_line_class}"><p>{quote_text}</p></div>"""
+                html_lines.append(quote_html)
+            else: # Normal text paragraphs
+                line_html = f"""<p>{line}</p>"""
+                html_lines.append(line_html)
+
+        # At the end, insert the date right below the title which should be the first line...
+        html_lines.insert(0, post_date_html)
+
+    return html_lines
+
+# Function for writing a blog post into an html file
+def generate_blog_post_html(post_title, post_description, html_lines):
     new_line = "\n"
     html_template = f"""<!DOCTYPE html>
     <html lang="en">
@@ -218,11 +260,12 @@ def generate_page(post_title, post_description, html_lines):
                         <a class="monospace button-call-to-action" href="mailto:website@shinjipons.com">Contact</a>
                     </div>
                 </nav>
-                <ul id="article-outline" class="monospace padding-between-items">{post_title}
+                <ul id="article-outline" class="monospace padding-between-items">Outline
                 </ul>
             </div>
             <div class="right-column">
                 <div class="blog-article-content">
+                    <h1>{post_title}</h1>
                     {f"{new_line.join(html_lines)}"}
                 </div>
             </div>
@@ -232,15 +275,16 @@ def generate_page(post_title, post_description, html_lines):
     </html>
     """
     post_filename = post_title.strip("\"").replace(" ", "-").lower()
-    output_path = os.path.join(OUTPUT_FOLDER, f"{post_filename}.html")
+    output_path = os.path.join(OUTPUT_FOLDER_BLOG_POSTS, f"{post_filename}.html")
 
     with open(output_path, "w") as html_file:
         html_file.write(html_template)
 
-# The actual action
+# The actual execution for making the blog post file
 for markdown_filepath in get_all_markdown_filepaths(markdown_src_directory):
     html_lines = []
     html_filename = markdown_filepath.split('/')[-1].strip('.md')
+    post_title = html_filename.replace("-", " ").capitalize()
     post_description = get_frontmatter_item(markdown_filepath, "description")
 
     # Replace the bold ** with <b> tags
@@ -259,48 +303,47 @@ for markdown_filepath in get_all_markdown_filepaths(markdown_src_directory):
         else:
             html_lines_b_a.append(line)
 
+    html_lines_b_a_i = []
+    for line in html_lines_b_a:
+        if "*" in line:
+            if "**" in line:
+                pass
+            else:
+                html_lines_b_a_i.append(replace_italics(line))
+        else: html_lines_b_a_i.append(line)
+
     # Wrapping the numbered and unordered list needs to happens outside of the main function, because they need to have "visibility" over the whole list
-    html_lines_b_a_ul = wrap_list_with_prefix(html_lines_b_a, f"\t<li class=\"{ul_list_item_class}\">", "<ul>")
-    html_lines_b_a_ul_ol = wrap_list_with_prefix(html_lines_b_a_ul, f"\t<li class=\"{ol_list_item_class}\">", "<ol>")
+    html_lines_b_a_i_ul = wrap_list_with_prefix(html_lines_b_a_i, f"\t<li class=\"{ul_list_item_class}\">", "<ul>")
+    html_lines_b_a_i_ul_ol = wrap_list_with_prefix(html_lines_b_a_i_ul, f"\t<li class=\"{ol_list_item_class}\">", "<ol>")
 
     # Now deal with the code block
-    html_lines_b_a_ul_ol_code = [] # Finished code!
+    html_lines_b_a_i_ul_ol_code = [] # Finished code!
     count = 0
-    for line in html_lines_b_a_ul_ol:
+    for line in html_lines_b_a_i_ul_ol:
         if line.lower() == "<p>```</p>":
             if count % 2 == 0: # it's an opening tag
                 new_line = f"""<div class=\"{code_block_class}\">"""
-                html_lines_b_a_ul_ol_code.append(new_line)
+                html_lines_b_a_i_ul_ol_code.append(new_line)
                 count += 1
             else: # it's a closing tag
                 new_line = f"""</div>"""
-                html_lines_b_a_ul_ol_code.append(new_line)
+                html_lines_b_a_i_ul_ol_code.append(new_line)
                 count += 1
         else: # don't do anything
-            html_lines_b_a_ul_ol_code.append(line)
+            html_lines_b_a_i_ul_ol_code.append(line)
 
     # Generate a single page
-    generate_page(html_filename, post_description, html_lines_b_a_ul_ol_code)
+    generate_blog_post_html(post_title, post_description, html_lines_b_a_i_ul_ol_code)
 
-# Function for making the blog.html page
+# ################# #
+#                   #
+#                   #
+# Visual separation #
+#                   #
+#                   #
+# ################# #
 
-def format_date(date_string):
-    year, month, day = date_string.split('-')
-    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-    month_name = months[int(month) - 1]
-
-    # Add original suffix to day
-    day_int = int(day)
-    if 4 <= day_int <= 20 or 24 <= day_int <= 30:
-        suffix = "th"
-    else:
-        suffix = ["st", "nd", "rd"][day_int % 10 - 1]
-
-    # format the final string
-    formatted_date = f"{month_name} {day_int}{suffix}, {year}"
-
-    return formatted_date
-
+# Function for making the html lines for linking to all the blog posts
 def link_blog_post_html_generator(source_directory):
     result = []
     post_title = ""
@@ -331,6 +374,7 @@ def link_blog_post_html_generator(source_directory):
 
     return result
 
+# Function for writing the blog.html file into dist/
 def blog_html_page_generator(html_lines):
     new_line = "\n"
     blog_html_template = f"""<!DOCTYPE html>
@@ -411,3 +455,11 @@ def blog_html_page_generator(html_lines):
         """
 
     # continue here
+    blog_html_filename = "blog".replace(" ", "-").lower()
+    output_path = os.path.join(OUTPUT_FOLDER_BLOG_INDEX, f"{blog_html_filename}.html")
+
+    with open(output_path, "w") as html_file:
+        html_file.write(blog_html_template)
+
+# The actual action
+blog_html_page_generator(link_blog_post_html_generator(markdown_src_directory))
